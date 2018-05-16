514ee8968f6ce61ed14fd31f02616257ee0d2cea
Saul <oliverasaul9201@gmail.com>
2018-05-16 16:41:17 -0400

    Casi terminado el reporte Contable

---
+++ Emplomania.UI/Model/AccountingReportModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emplomania.UI.Model
+{
+    public class AccountingReportModel
+    {
+        public int WorkerTurquesa { get; set; }
+        public int WorkerEsmeralda { get; set; }
+        public int WorkerZafiro { get; set; }
+        public int WorkerRubi { get; set; }
+        public int WorkerFichaDestacada { get; set; }
+        public int WorkerOfrezcoNecesito { get; set; }
+        public int WorkerTotal
+        {
+            get
+            {
+                int t = 0;
+                t += WorkerTurquesa;
+                t += WorkerEsmeralda;
+                t += WorkerZafiro;
+                t += WorkerRubi;
+                t += WorkerOfrezcoNecesito;
+                t += WorkerFichaDestacada;
+                return t;
+            }
+        }
+
+
+        public int EmployerTurquesa { get; set; }
+        public int EmployerEsmeralda { get; set; }
+        public int EmployerZafiro { get; set; }
+        public int EmployerRubi { get; set; }
+        public int EmployerOfrezcoNecesito { get; set; }
+        public int EmployerAsistenciaTecnica { get; set; }
+        public int EmployerTotal
+        {
+            get
+            {
+                int t = 0;
+                t += EmployerTurquesa;
+                t += EmployerEsmeralda;
+                t += EmployerZafiro;
+                t += EmployerRubi;
+                t += EmployerOfrezcoNecesito;
+                t += EmployerAsistenciaTecnica;
+                return t;
+            }
+        }
+
+
+        public int TeacherTurquesa { get; set; }
+        public int TeacherEsmeralda { get; set; }
+        public int TeacherZafiro { get; set; }
+        public int TeacherRubi { get; set; }
+        public int TeacherOfrezcoNecesito { get; set; }
+        public int TeacherConfeccionMatricula { get; set; }
+        public int TeacherTotal
+        {
+            get
+            {
+                int t = 0;
+                t += TeacherTurquesa;
+                t += TeacherEsmeralda;
+                t += TeacherZafiro;
+                t += TeacherRubi;
+                t += TeacherOfrezcoNecesito;
+                t += TeacherConfeccionMatricula;
+                return t;
+            }
+        }
+
+        public int TotalOficinaComercial { get; set; }
+        public int TotalDepositoBancario { get; set; }
+        public int Total
+        {
+            get
+            {
+                return TotalDepositoBancario + TotalOficinaComercial;
+            }
+        }
+
+
+    }
+}
--- Emplomania.UI/ViewModels/AccountingReportViewModels/AccountingReportViewModel.cs
+++ Emplomania.UI/ViewModels/AccountingReportViewModels/AccountingReportViewModel.cs
@@ -1,16 +1,36 @@
-﻿using Emplomania.UI.ViewModels.Base;
+﻿using Emplomania.UI.Infrastucture;
+using Emplomania.UI.Model;
+using Emplomania.UI.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Emplomania.UI.ViewModels.AccountingReportViewModels
 {
     public class AccountingReportViewModel : EMViewModelBase
     {
+        public DateTime? InitDate { get; set; }
+        public DateTime? FinalDate { get; set; }
+
+        private AccountingReportModel accountingReportModel;
+        public AccountingReportModel AccountingReportModel
+        {
+            get { return accountingReportModel; }
+            set { SetProperty(ref accountingReportModel, value); }
+        }
+
+        public ICommand SearchCommand => new RelayCommand(param =>
+        {
+            //TODO:Calcular aqui el reporte contable mediante IncomeService
+        });
+
+
         public AccountingReportViewModel(EMMainViewModel centralEMMain) : base(centralEMMain)
         {
+            AccountingReportModel = new AccountingReportModel();
         }
     }
 }
